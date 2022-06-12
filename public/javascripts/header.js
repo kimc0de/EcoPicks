6d390aa78115c8e25fcced70746442e22b63c071
kim <kimngan.ledang@zattoo.com>
2022-06-12 23:01:14 +0200

    header bottom category nav active styles

--- public/javascripts/header.js
+++ public/javascripts/header.js
@@ -1,5 +1,5 @@
 // Close menu toggler mobile
-const closeButton = document.getElementById("close-menu");
+const closeButton = document.getElementById('close-menu');
 const navbarContent = document.getElementById('navbarContent');
 const navbarToggler = document.getElementById('menu-button');
 
@@ -12,3 +12,18 @@ closeButton.addEventListener('click', function () {
         navbarContent.classList.remove('show');
     }
 })
+
+// Categories navigation - set active style for active link
+const headerBottom = document.getElementById('header-bottom');
+const categoryNav = headerBottom.getElementsByClassName('bottom-nav-link');
+
+for (let i = 0; i < categoryNav.length; i++) {
+    categoryNav[i].addEventListener("click", function(e) {
+        for (let j = 0; j < categoryNav.length; j++) {
+            if(categoryNav[j].classList.contains("active")){
+                categoryNav[j].className = categoryNav[j].className.replace(" active", "");
+            }
+        }
+        this.className += " active";
+    });
+}
