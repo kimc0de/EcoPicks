b74a5f3ce0bf244f24e9987c5e2dab3615b01821
kim <kimngan.ledang@zattoo.com>
2022-06-25 17:28:11 +0200

    show passwords

--- public/javascripts/brandCard.js
+++ public/javascripts/brandCard.js
@@ -1,9 +1,11 @@
 /**
  * Handle keyboard navigation and press enter on brand card
  */
-$(".card-body").keypress((e) => {
-    if(e.keyCode ===13) {
+$(() => {
+    $(".card-body").keypress((e) => {
+    if (e.keyCode === 13) {
         let brandId = $(event.target).data("id");
         window.location.href = `/brand/${brandId}`;
     }
 })
+});
