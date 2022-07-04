8cf78236d137614b4ee5682fdddf77dd271cd61a
kim <kimngan.ledang@zattoo.com>
2022-07-04 13:40:52 +0200

    refactor

--- public/javascripts/load-more.js
+++ public/javascripts/load-more.js
@@ -2,17 +2,19 @@ $(() => {
     $(window).on('load', () => {
         if (window.location.pathname.includes('/category/')) {
             let brandsList = $('#brandsByCategory .card-wrapper');
-            let loadMoreBtn = $('#load-more');
+            let loadMoreBtn = $('#brandsByCategory .load-more');
             // Show the first results
             brandsList.slice(0,9).show();
 
             loadMoreBtn.on('click', () => {
-                $('#brandsByCategory .card-wrapper:hidden').slice(0,15).show();
+                let hiddenBrands = $('#brandsByCategory .card-wrapper:hidden');
+                hiddenBrands.slice(0,15).show();
 
-                if($('#brandsByCategory .card-wrapper:hidden').length === 0) {
+                if(hiddenBrands.length === 0) {
                     loadMoreBtn.fadeOut();
                 }
             })
         }
     })
 })
+
