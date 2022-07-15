7a9d3411ebb7af372dbfa1ccbaeb46752bae29af
kim <kimngan.ledang@zattoo.com>
2022-07-15 23:09:35 +0200

    scrollTop

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,13 +1,12 @@
-// https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_scroll_to_top
 /**
  * Scroll to top button
  */
 $(function(){
-    //Get the button
     let backToTopBtn = $("#back-to-top");
 
+    // Show and hide the button depending on how much user scrolls
     $(window).scroll(function() {
-        if (document.body.scrollTop > 1000 || document.documentElement.scrollTop > 1000) {
+        if ($(window).scrollTop() > 1000 || $(window).scrollTop() > 1000) {
             backToTopBtn.removeClass('d-none');
             backToTopBtn.addClass('d-block');
         } else {
@@ -15,10 +14,10 @@ $(function(){
             backToTopBtn.addClass('d-none');
         }
     });
-    // When the user clicks on the button, scroll to the top of the document
+    // When the user clicks on the button, scroll to the top of the document, focus on skip-link
     backToTopBtn.on('click',() => {
-        document.body.scrollTop = 0;
-        document.documentElement.scrollTop = 0;
+        $('#skip-link').focus();
+        $(window).scrollTop(0);
     });
 });
 
