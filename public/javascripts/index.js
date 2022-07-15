3d9efc8ffa3862b187fc5f91e55a7c4e879b9f4f
kim <kimngan.ledang@zattoo.com>
2022-07-15 22:53:11 +0200

    naming

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,20 +1,22 @@
-
-// When the user scrolls down 20px from the top of the document, show the button
+// https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_scroll_to_top
+/**
+ * Scroll to top button
+ */
 $(function(){
     //Get the button
-    let mybutton = $("#back-to-top");
+    let backToTopBtn = $("#back-to-top");
 
     $(window).scroll(function() {
         if (document.body.scrollTop > 1000 || document.documentElement.scrollTop > 1000) {
-            mybutton.removeClass('d-none');
-            mybutton.addClass('d-block');
+            backToTopBtn.removeClass('d-none');
+            backToTopBtn.addClass('d-block');
         } else {
-            mybutton.removeClass('d-block');
-            mybutton.addClass('d-none');
+            backToTopBtn.removeClass('d-block');
+            backToTopBtn.addClass('d-none');
         }
     });
     // When the user clicks on the button, scroll to the top of the document
-    mybutton.on('click',() => {
+    backToTopBtn.on('click',() => {
         document.body.scrollTop = 0;
         document.documentElement.scrollTop = 0;
     });
